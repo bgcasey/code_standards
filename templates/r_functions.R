e3398c70063feab6b96611177dcc6bbdc4ba172e
bgcasey <bgcasey@ualberta.ca>
2024-07-25 06:46:50 -0600

    initial functions template

---
+++ templates/r_functions.R
@@ -0,0 +1,48 @@
+# ---
+# title: "R functions template"
+# author: "Brendan Casey"
+# created: "2024-07-25"
+# description: 
+#   "Template for creating properly formatted and  
+#   documented R functions"
+# ---
+
+
+#' [Title of the Function]
+#'
+#' [Brief description of what the function does]
+#'
+#' @param [param_name] [Type and description of the parameter]
+#' @param [param_name] [Type and description of the parameter]
+#' @param [param_name] [Type and description of the parameter]
+#' @return [Description of the return value or object]
+#' 
+#' @example # Example usage of the function
+#' [Example data creation]
+#' [Example function call]
+#' [Example result printing]
+[function_name] <- function([param1], [param2], 
+                            [param3] = [default_value], 
+                            [param4]) {
+  # [Step 1: Description of what this step does]
+  [code for step 1]
+  
+  # [Step 2: Description of what this step does]
+  if (!is.null([param3])) {
+    if (!is.numeric([param3])) {
+      stop("[Error message if param3 is not numeric]")
+    }
+    [code for step 2]
+  }
+  
+  # [Step 3: Description of what this step does]
+  [code for step 3]
+  
+  # [Step 4: Description of what this step does]
+  [code for step 4]
+  
+  # [Step 5: Description of what this step does]
+  [code for step 5]
+  
+  return([result])
+}
