2d4f6c62f45bd0046db6097f93ca8fa8cb11fb6b
Brendan Casey <brendancasey@abmi.ualberta.ca>
2024-12-13 08:02:37 -0500

    JavaScript function template

---
+++ templates/javascript_function.js
@@ -0,0 +1,43 @@
+/**
+ * [Title of the Function]
+ * 
+ * [Brief description of what the function does]
+ * 
+ * @param {Type} param1 - [Description of param1]
+ * @param {Type} param2 - [Description of param2]
+ * @param {Type, Optional} [param3=defaultValue] - [Description of 
+ * param3]
+ * @param {Type} param4 - [Description of param4]
+ * @return {Type} [Description of the return value or object]
+ * 
+ * @example
+ * // Example usage of the function
+ * var exampleData = [example data];
+ * var result = functionName(param1, param2, param3, param4);
+ * print(result);
+ */
+exports.functionName = function functionName(
+  param1,
+  param2,
+  param3 = defaultValue,
+  param4
+) {
+  // Step 1: [Description of what this step does]
+  var step1Result = [code for step 1];
+  
+  // Step 2: [Description of what this step does]
+  var step2Result = [code for step 2];
+  
+  // Step 3: [Description of what this step does]
+  var step3Result = [code for step 3];
+  
+  // Step 4: [Description of what this step does]
+  var step4Result = [code for step 4];
+  
+  // Step 5: [Description of what this step does]
+  var finalResult = [code for step 5];
+  
+  return finalResult;
+};
+
+
